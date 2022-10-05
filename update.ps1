00841e542cdd5adcb75295ed2c101c0dd85d7370
Zoull <zoull@zdindustries.com>
2022-10-05 16:41:36 -0600

    Initial commit

---
+++ update.ps1
@@ -0,0 +1,120 @@
+$ErrorActionPreference = 'Stop'
+
+$razerSynapseUrl = 'http://rzr.to/synapse-3-pc-download'
+
+Add-Content -Path '.\update.log' -Value "[$(Get-Date)] Checking for update"
+
+# Start capture of remote version
+$response = try {
+    Invoke-WebRequest -Uri $razerSynapseUrl -UseBasicParsing
+} catch {
+    $_.Exception.Response
+}
+
+if (-not $response.BaseResponse.ResponseUri.AbsoluteUri) {
+    Add-Content -Path '.\update.log' -Value "[$(Get-Date)] No redirection uri returned when requesting $razerSynapseUrl"
+    return
+}
+
+$downloadUrl = $response.BaseResponse.ResponseUri.AbsoluteUri
+if (-not ($downloadUrl -match '.*_V(?<RemoteVersion>.*)\.exe')) {
+    Add-Content -Path '.\update.log' -Value "[$(Get-Date)] Could not determine version from url $downloadUrl"
+    return
+}
+$remoteVersion = $matches.RemoteVersion
+$remoteFilename = [System.IO.Path]::GetFileName($downloadUrl)
+
+# Start capture of local version
+$localVersion = $null
+if (Test-Path -Path '.\files\web-installer\*.exe') {
+    $localFileInfo = Get-ItemProperty -Path '.\files\web-installer\*'
+    if (-not ($localFileInfo.Name -match '.*_V(?<LocalVersion>.*)\.exe')) {
+        Add-Content -Path '.\update.log' -Value "[$(Get-Date)] Could not determine version from local file $($localFileInfo.Name)"
+        return
+    }
+    $localVersion = $matches.LocalVersion
+}
+
+# Check remote version against local version
+if ($localVersion -eq $null -or [System.Version]$remoteVersion -gt [System.Version]$localVersion) {
+    # Update found
+    if ($localVersion -ne $null) {
+        Add-Content -Path '.\update.log' -Value "[$(Get-Date)] Updating from version $localVersion to version $remoteVersion"
+
+        Remove-Item -Path '.\files\web-installer\*'
+    } else {
+        Add-Content -Path '.\update.log' -Value "[$(Get-Date)] Updating to initial version $remoteVersion"
+    }
+
+    # Start install of web installer to get components
+    Invoke-WebRequest -Uri $downloadUrl -Outfile $remoteFilename -UseBasicParsing
+    Move-Item -Path $remoteFilename -Destination '.\files\web-installer'
+
+    Start-Process 'AutoHotKey' 'web-install.ahk'
+    Start-Process ".\files\web-installer\$remoteFilename" -Wait
+
+    Remove-Item -Path '.\files/components\*'
+
+    Copy-Item -Path 'C:\Windows\Installer\Razer\Installer\*_*.exe' -Destination '.\files\components' -Recurse
+
+    # Get information from each component
+    $components = @()
+    $componentInfos = Get-ItemProperty -Path '.\files\components\*'
+    foreach ($componentInfo in $componentInfos) {
+        if (-not ($componentInfo.Name -match ".*(?<ComponentName>Razer\S*|GMS\S*Setup)_.*")) {
+            Add-Content -Path '.\update.log' -Value "[$(Get-Date)] Could not determine component name from $($componentInfo.Name)"
+        }
+        $componentName = $matches.ComponentName.replace('_','')
+
+        $components += @{
+            Name = $componentName;
+            Link = "https://cdn.razersynapse.com/$($componentInfo.Name)";
+            Hash = (Get-FileHash -Path ".\files\components\$($componentInfo.Name)" -Algorithm 'sha256').Hash;
+        }
+    }
+
+    # Update chocolatey-packages repo
+    Push-Location '.\chocolatey-packages'
+    git pull
+    Pop-Location
+
+    # Prep urls and checksums to be replaced in chocolatey install script
+    $componentReplaceValues = @{}
+    foreach ($component in $components) {
+        $componentReplaceValues += @{"(?i)(^[$]$($component.Name)Url\s*=\s*)'.*'"      = "`${1}'$($component.Link)'"}
+        $componentReplaceValues += @{"(?i)(^[$]$($component.Name)Checksum\s*=\s*)'.*'" = "`${1}'$($component.Hash)'"}
+    }
+
+    # Replace urls and checksums in chocolatey install script
+    $chocolateyInstallScriptFilename = '.\chocolatey-packages\razer-synapse-3\tools\chocolateyInstall.ps1'
+    $fileContent = Get-Content $chocolateyInstallScriptFileName -Encoding UTF8
+    $componentReplaceValues.GetEnumerator() | ForEach-Object {
+        if (!($fileContent -match $_.name)) { throw "Search pattern not found: '$($_.name)'" }
+        $fileContent = $fileContent -replace $_.name, $_.value
+    }
+    $utf8BomEncoding = New-Object System.Text.UTF8Encoding($true)
+    $output = $fileContent | Out-String
+    [System.IO.File]::WriteAllText($chocolateyInstallScriptFilename, $output, $utf8BomEncoding)
+
+    # Replace version in nuspec file with new version
+    $nuspecFile = '.\chocolatey-packages\razer-synapse-3\razer-synapse-3.nuspec'
+    $nuspec = New-Object xml
+    $nuspec.PSBase.PreserveWhitespace = $true
+    $nuspec.Load($nuspecFile)
+    $nuspec.package.metadata.version = $remoteVersion
+    $utf8NoBomEncoding = New-Object System.Text.UTF8Encoding($false)
+    [System.IO.File]::WriteAllText($nuspecFile, $nuspec.InnerXml, $utf8NoBomEncoding)
+
+    # Commit update to chocolatey-packages repo
+    Push-Location '.\chocolatey-packages'
+    git add .
+    git commit -m "Update Razer Synapse 3 chocolatey package to version $remoteVersion (auto)"
+    git push
+    Pop-Location
+
+    Add-Content -Path '.\update.log' -Value "[$(Get-Date)] Finished updating to version $remoteVersion"
+    Add-Content -Path '.\update.log' -Value "--------------------------------------------------------------------------------"
+} else {
+    Add-Content -Path '.\update.log' -Value "[$(Get-Date)] No update found"
+    Add-Content -Path '.\update.log' -Value "--------------------------------------------------------------------------------"
+}
---
+++ web-install.ahk
@@ -0,0 +1,52 @@
+﻿#NoEnv ; Recommended for performance and compatibility with future AutoHotkey releases.
+; #NoTrayIcon
+#Warn ; Enable warnings to assist with detecting common errors.
+SendMode Input ; Recommended for new scripts due to its superior speed and reliability.
+SetTitleMatchMode, 1 ; A windows's title must start with the specified WinTitle to be a match.
+SetControlDelay 0 
+SetWorkingDir %A_ScriptDir% ; Ensures a consistent starting directory.
+
+winTitle = ahk_exe RazerInstaller.exe
+
+; Splash screen
+; Wait for it to go away
+WinWait, %winTitle%,, 120
+Sleep, 25000
+
+; Select software to install
+; Only Razer Synapse by default
+WinWait, %winTitle%,, 15
+If WinExist(winTitle)
+{
+    WinActivate
+    Send {Enter} ; Install
+    Sleep, 4000 ; Wait for transition to finish
+}
+
+; Razer Cortex promotion
+WinWait, %winTitle%,, 15
+If WinExist(winTitle)
+{
+    WinActivate
+    Send {Esc} ; Skip and Continue
+    Sleep, 4000 ; Wait for next screen to fully load
+}
+
+; Installation
+; Wait for it to finish
+WinWait, %winTitle%,, 15
+Sleep, 210000
+
+; Congratulations, Get Started screen
+WinWait, %winTitle%,, 15
+If WinExist(winTitle)
+{
+    WinActivate
+    Send {Tab}
+    Send {Tab}
+    Send {Space} ; Stop Razer Synapse from starting on finish
+    Send {Tab}
+    Send {Enter} ; Get Started
+}
+
+Exit
